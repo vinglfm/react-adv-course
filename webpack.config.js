dad77b7580387bacde91b809a3f34987278aa007
Andrew Pryshchepa <vinglfm@gmail.com>
2018-08-11 09:30:39 +0300

    Preparing app for production

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,40 @@ module.exports = {
   resolve: {
     modules: [path.resolve('./lib'), path.resolve('./node_modules')]
   },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  // entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  entry: {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'axios',
+      'lodash.debounce',
+      'lodash.pickby'
+    ],
+    app: ['./lib/renderers/dom.js']
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
-    rules: [
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' }
+    rules: [ 
+      { 
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/, 
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env', 'stage-2']
+          }
+        }
+      }
     ]
+  },  
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
   }
 };
